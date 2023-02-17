b911cf9168705abe6c3a520922dcf819d2e5d3a1
yunggenius <yunggenius4@gmail.com>
2023-02-17 13:26:59 +0100

    ens and account balance

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,13 +1,34 @@
 import { useAccount, useEnsName } from 'wagmi'
+import { useEffect, useState } from 'react'
+import { fetchBalance } from '@wagmi/core'
 
 export function Account() {
   const { address } = useAccount()
   const { data: ensName } = useEnsName({ address })
+  const [addy, setAddy] = useState("")
+  const [bal, setBal] = useState('')
+  // const {address} = Account()
+
+  useEffect(() => {
+    const getBalance = async () => {
+      const balance = await fetchBalance({
+        address: address,
+      })
+      setBal(balance.formatted)
+    }
+    getBalance()
+      .catch(console.error);
+    // console.log(bal)
+  })
 
   return (
     <div>
-      {ensName ?? address}
+      {/* {ensName ?? address} */}
       {ensName ? ` (${address})` : null}
+      <br />
+      Account Balance: {bal} ETH 
+      <br />
+      ENS name: {ensName}
     </div>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,63 @@
-import { useAccount } from 'wagmi'
+import { useEffect, useState } from 'react'
+import { useAccount, useEnsName } from 'wagmi'
+import { fetchBalance, fetchEnsName } from '@wagmi/core'
 
 import { Account, Connect, NetworkSwitcher } from '../components'
 
+
 function Page() {
-  const { isConnected } = useAccount()
+  const { isConnected, address } = useAccount()
+  const [addy, setAddy] = useState("")
+  const [bals, setBals] = useState('')
+  const [ens, setEnsName] = useState('')
+  // const {address} = Account()
+
+  useEffect(() => {
+    const getBalance = async () => {
+      const balance = await fetchBalance({
+        address: addy,
+      })
+      //get ens name
+      const ensName = await fetchEnsName({
+        address: addy,
+      })
+      setEnsName(ensName)
+      setBals(balance.formatted)
+    }
+    getBalance()
+      .catch(console.error);
+    // console.log(bal)
+  })
+
 
   return (
     <>
       <h1>wagmi + Next.js</h1>
-
       <Connect />
-
       {isConnected && (
         <>
           <Account />
           <NetworkSwitcher />
         </>
+
       )}
+
+      <br />
+
+      <form onSubmit={(e) => {
+        e.preventDefault()
+        // setAddy(e.target.value)
+        // sendTransaction?.()
+      }}>
+        <input
+          onChange={(e) => setAddy(e.target.value)}
+          aria-label="Account" placeholder="0xA0Cf…251e" />
+        <button>fetch balance</button>
+        <br />
+        Account Balance: {bals} ETH
+      </form>
+      ENS Name: {ens}
+
     </>
   )
 }
